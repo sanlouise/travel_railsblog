b647f3e42c901807fe8e0126555c3c9f1a521d81
sanlouise <sandra.adamshallie@gmail.com>
2016-01-14 16:22:09 +0000

    Create new template, action and route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+  
+  private
+  
+    def article_params
+      params.require(:article).permit(:title, :city, :country, :description)
+    end
+    
+  
 end
 
