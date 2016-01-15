2cb3fd62aa43fedc9e1624b65ee4a7d083242fdd
sanlouise <sandra.adamshallie@gmail.com>
2016-01-15 10:25:54 +0000

    Update article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :city, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :country, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :country, presence: true, length: { minimum: 2, maximum: 50 }
   validates :description, presence: true, length: { minimum: 30 }
   
 end
