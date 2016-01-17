177cfb8cb7bc43cc32d1d68149dcb83332a34c11
sanlouise <sandra.adamshallie@gmail.com>
2016-01-17 12:05:23 +0000

    Remove redundancy in users contrroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 6)
   end
@@ -19,12 +19,12 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+   
   end
   
   
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully!"
       redirect_to articles_path
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 6)
   end
   
@@ -43,6 +43,11 @@ class UsersController < ApplicationController
   end
   
   private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+   
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
