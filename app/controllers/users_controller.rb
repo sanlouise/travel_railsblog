ca31d5c70eddfbe5d57ff332a0fa94ec68978d67
sanlouise <sandra.adamshallie@gmail.com>
2016-01-17 14:04:24 +0000

    Add admin functionality in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 6)
@@ -43,6 +44,9 @@ class UsersController < ApplicationController
   end
   
   def destroy
+    @user.destroy
+    flash[:danger] = "The user and all associated stories have been deleted successfully."
+    redirect_to users_path
   end
   
   private
@@ -56,10 +60,17 @@ class UsersController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own settings."
       redirect_to root_path
     end
   end
+  
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admins can do this."
+    end
+  end
+  
     
 end
