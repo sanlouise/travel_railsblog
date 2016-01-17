3364843c809dbff26b95d3f01284b18011b4c209
sanlouise <sandra.adamshallie@gmail.com>
2016-01-17 11:39:20 +0000

    Create user sessions login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,22 @@ class SessionsController < ApplicationController
   end
   
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email.downcase])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}! You look great today!"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Oops! It seems that this email and password do not match data in our system. Please try again."
+      render 'new'
+    end
+    
   end
   
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You are now logged out."
+    redirect_to root_path
   end
 
   
