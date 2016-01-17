7367afebc661e3ce652536c114f537c8b0ce2187
sanlouise <sandra.adamshallie@gmail.com>
2016-01-17 12:44:06 +0000

    Update user authentication in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 6)
   end
@@ -51,4 +53,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own settings."
+      redirect_to root_path
+    end
+  end
+    
 end
