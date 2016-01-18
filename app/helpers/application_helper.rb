f9d137902fa01e7f1fd285112e151d9cbe390079
sanlouise <sandra.adamshallie@gmail.com>
2016-01-18 19:15:25 +0000

    Update css user show page and bg responsiveness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   
-  def gravatar_for(user, options = { size: 80})
+  def gravatar_for(user, options = { size: 150})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
