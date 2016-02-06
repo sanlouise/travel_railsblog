f5343954aab2238c8d666cf99c242e080639291a
sanlouise <sandra.adamshallie@gmail.com>
2016-02-06 11:44:41 +0000

    Create messages controller, partial and route

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   resources :categories, except: [:destroy]
   
-
+  resources :conversations do
+    resources :messages
+  end
 
  
   # The priority is based upon order of creation: first created -> highest priority.
